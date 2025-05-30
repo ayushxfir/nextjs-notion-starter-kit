e5c7a647d9e57cafc339b19e93c38c4d55abf7ad
ayushxfir <piyushartist00@gmail.com>
2025-05-31 01:16:26 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  rootNotionPageId: '2019a5ade2b3806cbbc4f7b82d009d53',
+
+  name: 'Xyush',
+  domain: 'fruition-xyush.vercel.app',
+  author: 'Xyush',
+
+  description: 'Indian Phonk Artist Xyush Official Notion Website',
+  socialImageTitle: 'Xyush',
+  socialImageSubtitle: '🔥 Indian Phonk Revolution',
+  github: 'ayushxfir',
+  linkedin: '',
+  twitter: '',
+
+  defaultPageIcon: null,
+  defaultPageCover: null,
+  defaultPageCoverPosition: 0.5,
+
+  isPreviewImageSupportEnabled: false,
+  isRedisEnabled: false,
+  pageUrlOverrides: null,
+  navigationStyle: 'default'
+};
